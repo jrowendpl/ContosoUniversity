0c454d132dbdc455c1b2b1137cd85401df9c73c2
jrowendpl <jrowen@dontpaniclabs.com>
2018-05-18 09:48:01 -0500

    Create a complex data model

--- ContosoUniversity/Migrations/20180518143748_ComplexDataModel.Designer.cs
+++ ContosoUniversity/Migrations/20180518143748_ComplexDataModel.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace ContosoUniversity.Migrations
 {
     [DbContext(typeof(SchoolContext))]
-    partial class SchoolContextModelSnapshot : ModelSnapshot
+    [Migration("20180518143748_ComplexDataMode1")]
+    partial class ComplexDataMode1
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
